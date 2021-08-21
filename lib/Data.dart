9c9a3d9e5b431314f49937db8649138118fd7d48
Enjyyy <bluestacksdemo9@gmail.com>
2021-08-22 00:28:51 +0200

    fixed urls

--- lib/Data.dart
+++ lib/Data.dart
@@ -15,81 +15,99 @@ List<ArticleDiv> politicsArticles = [];
 List<ArticleDiv> sportsArticles = [];
 List<ArticleDiv> scienceTechArticles = [];
 var choosenCategory;
+List<String> newsUrls = [
+  'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ee4702e3048d4180a85690b373d48334',
+  'https://newsapi.org/v2/top-headlines/sources?apiKey=ee4702e3048d4180a85690b373d48334',
+  'https://newsapi.org/v2/top-headlines/country=eg&apiKey=ee4702e3048d4180a85690b373d48334'
+];
 Future<void> getNews() async {
-  String url =
-      'https://newsapi.org/v2/everything?q=tesla&from=2021-07-18&sortBy=publishedAt&apiKey=ee4702e3048d4180a85690b373d48334';
-  var response = await http.get(Uri.parse(url));
-  var jsonData = jsonDecode(response.body);
-  if (jsonData["status"] == "ok") {
-    jsonData["articles"].forEach((div) {
-      ArticleDiv article = ArticleDiv(
+  newsUrls.forEach((url) async {
+    var response = await http.get(Uri.parse(url));
+    var jsonData = jsonDecode(response.body);
+    if (jsonData["status"] == "ok") {
+      jsonData["articles"].forEach((div) {
+        ArticleDiv article = ArticleDiv(
           imgSrc: div["urlToImage"].toString(),
           info: div["content"].toString(),
           header: div["title"].toString(),
           author: div["author"].toString(),
-          urll: div["url"].toString());
-      articles.add(article);
-      print(articles[articles.length - 1].author);
-    });
-  }
+          urll: div["url"].toString(),
+        );
+        articles.add(article);
+        print(articles[articles.length - 1].author);
+      });
+    }
+  });
 }
 
+List<String> politicsNewsUrls = [
+  'https://newsapi.org/v2/top-headlines?q=trump&apiKey=ee4702e3048d4180a85690b373d48334',
+  ' https://newsapi.org/v2/top-headlines?country=eg&apiKey=Aee4702e3048d4180a85690b373d48334'
+];
 Future<void> getPoliticsNews() async {
-  String url =
-      'https://newsapi.org/v2/everything?q=tesla&from=2021-07-18&sortBy=publishedAt&apiKey=ee4702e3048d4180a85690b373d48334';
-  var response = await http.get(Uri.parse(url));
-  var jsonData = jsonDecode(response.body);
-  if (jsonData["status"] == "ok") {
-    jsonData["articles"].forEach((div) {
-      ArticleDiv article = ArticleDiv(
-          imgSrc: div["urlToImage"].toString(),
-          info: div["content"].toString(),
-          header: div["title"].toString(),
-          author: div["author"].toString(),
-          urll: div["url"].toString());
-      politicsArticles.add(article);
-      print(politicsArticles[politicsArticles.length - 1].author);
-    });
-  }
+  politicsNewsUrls.forEach((url) async {
+    var response = await http.get(Uri.parse(url));
+    var jsonData = jsonDecode(response.body);
+    if (jsonData["status"] == "ok") {
+      jsonData["articles"].forEach((div) {
+        ArticleDiv article = ArticleDiv(
+            imgSrc: div["urlToImage"].toString(),
+            info: div["content"].toString(),
+            header: div["title"].toString(),
+            author: div["author"].toString(),
+            urll: div["url"].toString());
+        politicsArticles.add(article);
+        print(politicsArticles[politicsArticles.length - 1].author);
+      });
+    }
+  });
 }
 
+List<String> sportsNewsUrls = [
+  'https://newsapi.org/v2/top-headlines?country=de&category=sports&apiKey=ee4702e3048d4180a85690b373d48334',
+  'https://newsapi.org/v2/top-headlines?country=eg&category=sports&apiKey=ee4702e3048d4180a85690b373d48334'
+];
 Future<void> getSportsNews() async {
-  String url =
-      'https://newsapi.org/v2/everything?q=tesla&from=2021-07-18&sortBy=publishedAt&apiKey=ee4702e3048d4180a85690b373d48334';
-  var response = await http.get(Uri.parse(url));
-  var jsonData = jsonDecode(response.body);
-  if (jsonData["status"] == "ok") {
-    jsonData["articles"].forEach((div) {
-      ArticleDiv article = ArticleDiv(
+  sportsNewsUrls.forEach((url) async {
+    var response = await http.get(Uri.parse(url));
+    var jsonData = jsonDecode(response.body);
+    if (jsonData["status"] == "ok") {
+      jsonData["articles"].forEach((div) {
+        ArticleDiv article = ArticleDiv(
+            imgSrc: div["urlToImage"].toString(),
+            info: div["content"].toString(),
+            header: div["title"].toString(),
+            author: div["author"].toString(),
+            urll: div["url"].toString());
+        sportsArticles.add(article);
+        print(sportsArticles[sportsArticles.length - 1].author);
+      });
+    }
+  });
+}
+
+List<String> scienceTechUrls = [
+  'https://newsapi.org/v2/everything?q=tesla&from=2021-07-21&sortBy=publishedAt&apiKey=ee4702e3048d4180a85690b373d48334',
+  'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ee4702e3048d4180a85690b373d48334',
+];
+Future<void> getScienceTechNews() async {
+  scienceTechUrls.forEach((url) async {
+    var response = await http.get(Uri.parse(url));
+    var jsonData = jsonDecode(response.body);
+    if (jsonData["status"] == "ok") {
+      jsonData["articles"].forEach((div) {
+        ArticleDiv article = ArticleDiv(
           imgSrc: div["urlToImage"].toString(),
           info: div["content"].toString(),
           header: div["title"].toString(),
           author: div["author"].toString(),
-          urll: div["url"].toString());
-      sportsArticles.add(article);
-      print(sportsArticles[sportsArticles.length - 1].author);
-    });
-  }
-}
-
-Future<void> getScienceTechNews() async {
-  String url =
-      'https://newsapi.org/v2/everything?q=tesla&from=2021-07-18&sortBy=publishedAt&apiKey=ee4702e3048d4180a85690b373d48334';
-  var response = await http.get(Uri.parse(url));
-  var jsonData = jsonDecode(response.body);
-  if (jsonData["status"] == "ok") {
-    jsonData["articles"].forEach((div) {
-      ArticleDiv article = ArticleDiv(
-        imgSrc: div["urlToImage"].toString(),
-        info: div["content"].toString(),
-        header: div["title"].toString(),
-        author: div["author"].toString(),
-        urll: div["url"].toString(),
-      );
-      scienceTechArticles.add(article);
-      print(scienceTechArticles[scienceTechArticles.length - 1].author);
-    });
-  }
+          urll: div["url"].toString(),
+        );
+        scienceTechArticles.add(article);
+        print(scienceTechArticles[scienceTechArticles.length - 1].author);
+      });
+    }
+  });
 }
 
 launchURL(String url) async {
--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -15,7 +15,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
-    // TODO: implement initState
     setState(() {
       getNews();
     });
